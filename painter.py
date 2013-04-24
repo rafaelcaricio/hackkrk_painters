403f9b3541c9ffeb4c5168eef7cabb07edf0c20e
Rafael Caricio <rafael.jacinto@gmail.com>
2013-04-24 23:24:17 +0200

    send avatar.

--- painter.py
+++ painter.py
@@ -43,21 +43,35 @@ class ImageFinder(object):
         raise NotImplementedError
 
     def convert_to_base64(self, image):
-        return base64.b64encode(image)
+        byte_array = StringIO()
+        image.save(byte_array, format='PNG')
+        result = byte_array.getvalue()
+        byte_array.close()
+        return base64.b64encode(result)
 
 class PILImageGenerator(ImageFinder):
     def get_image_with_color(self, r, g, b):
         image_object = Image.new('RGB', (64, 64), (r, g, b))
-        byte_array = StringIO()
-        image_object.save(byte_array, format='PNG')
-        result = byte_array.getvalue()
-        byte_array.close()
-        return self.convert_to_base64(result)
+        return self.convert_to_base64(image_object)
+
+
+class FromPictureAlphaGenerator(ImageFinder):
+    def __init__(self, file_name):
+        im = Image.open(file_name)
+        self.image = im.convert('RGB')
+        self.image.thumbnail((64,64), Image.ANTIALIAS)
+
+    def get_image_with_color(self, r, g, b):
+        background = Image.new('RGB', (64, 64), (r, g, b))
+        output = Image.blend(self.image, background, 0.75)
+        output.thumbnail((64,64), Image.ANTIALIAS)
+        output.save('result.png', format='PNG')
+        return self.convert_to_base64(output)
 
 if __name__ == '__main__':
     chlgs = ChallengeService(token=os.environ.get('HACKKRK_TOKEN', ''))
     current_challenge = chlgs.get_new_challenge()
     print 'Got the challenge: %s' % dumps(current_challenge)
-    base64_image = PILImageGenerator().get_image_with_color(*current_challenge['color'])
+    base64_image = FromPictureAlphaGenerator('rafael.jpg').get_image_with_color(*current_challenge['color'])
     print 'Image generated: %s' % base64_image
     print chlgs.solve(current_challenge, base64_image)
